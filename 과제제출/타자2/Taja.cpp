8b2c1bcb5c78ee9b11790f95d11a7300a27b496a
ParkSoHyeon <zxasd9874@gmail.com>
2020-05-12 13:04:00 +0900

    asd

--- 과제제출/타자2/Taja.cpp
+++ 과제제출/타자2/Taja.cpp
@@ -4,8 +4,8 @@
 int main()
 {
 	_CrtSetDbgFlag(_CRTDBG_LEAK_CHECK_DF | _CRTDBG_ALLOC_MEM_DF);
-	_CrtSetBreakAlloc(70390);
-	_CrtSetBreakAlloc(308); //대체 뭐가 메모리 누수 발생시키는지 모르겠음
+	//_CrtSetBreakAlloc(70390);
+	//_CrtSetBreakAlloc(308); //대체 뭐가 메모리 누수 발생시키는지 모르겠음
 	//_CrtSetBreakAlloc(155);
 	//_CrtSetBreakAlloc(156);
 	//_CrtSetBreakAlloc(157);
