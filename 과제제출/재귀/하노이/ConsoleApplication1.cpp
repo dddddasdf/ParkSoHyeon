b042b43eb338465945cdafe9e9e62f5e9ab0f65d
ParkSoHyeon <zxasd9874@gmail.com>
2020-05-18 13:24:22 +0900

    d

---
+++ 과제제출/재귀/하노이/ConsoleApplication1.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+
+void Hanoi(int number, char from, char tmp, char to)
+{
+	if (number == 1)
+		std::cout << "원반1을 " << from << "에서 " << to << "로 이동\n";
+	else
+	{
+		Hanoi(number - 1, from, to, tmp);
+		std::cout << "원반" << number << "를 " << from << "에서 " << to << "로 이동\n";
+		Hanoi(number - 1, tmp, from, to);
+	}
+}
+
+int main()
+{
+	char from = 'A';
+	char tmp = 'B';
+	char to = 'C';
+
+	Hanoi(4, from, tmp, to);
+
+
+
+	return 0;
+}
