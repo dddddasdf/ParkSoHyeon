9e288e219283964612350124446bbee298e0fca0
ParkSoHyeon <zxasd9874@gmail.com>
2020-05-25 13:46:54 +0900

    df

--- 과제제출/리스트/배열리스트4/main.c
+++ 과제제출/리스트/배열리스트4/main.c
@@ -4,7 +4,7 @@
 #include <stdlib.h>
 #include <string.h>
 
-void InputInformation(NameCard Array[], int Size)
+void InputInformation(NameCard *Array, int Size)
 {
 	char NameTmp[NAME_LEN];
 	char PhoneTmp[PHONE_LEN];
@@ -24,7 +24,7 @@ void InputInformation(NameCard Array[], int Size)
 	}
 }
 
-void SearchInformation(NameCard Array[], int Size)
+void SearchInformation(NameCard *Array, int Size)
 {
 	printf("검색할 이름 입력: ");
 	char NameSearch[NAME_LEN];
@@ -41,7 +41,7 @@ void SearchInformation(NameCard Array[], int Size)
 	printf("해당하는 이름을 가진 사람이 없습니다.\n");
 }
 
-void SearchPhoneNumber(NameCard *Array[], int Size)
+void SearchPhoneNumber(NameCard *Array, int Size)
 {
 	printf("검색할 이름 입력: ");
 	char NameSearch[NAME_LEN];
@@ -49,19 +49,19 @@ void SearchPhoneNumber(NameCard *Array[], int Size)
 
 	for (int i = 0; i < Size; i++)
 	{
-		if (NameCompare(Array[i], NameSearch) == 0)
+		if (NameCompare(&Array[i], NameSearch) == 0)
 		{
 			char PhoneNumberTmp[PHONE_LEN];
 			printf("변경할 전화번호를 입력: ");
 			scanf("%s", PhoneNumberTmp);
-			ChangePhoneNum(Array[i], PhoneNumberTmp);
+			ChangePhoneNum(&Array[i], PhoneNumberTmp);
 			return;
 		}
 	}
 	printf("해당하는 이름을 가진 사람이 없습니다.\n");
 }
 
-void DeleteInformation(NameCard *Array[], int *Size)
+void DeleteInformation(NameCard *Array, int *Size)
 {
 	printf("검색할 이름 입력: ");
 	char NameSearch[NAME_LEN];
@@ -69,29 +69,31 @@ void DeleteInformation(NameCard *Array[], int *Size)
 
 	for (int i = 0; i < *Size; i++)
 	{
-		if (NameCompare(Array[i], NameSearch) == 0)
+		if (NameCompare(&Array[i], NameSearch) == 0)
 		{
-			for (int j = i; j < 3; j++)
+			for (int j = i; j < *Size; j++)
 			{
-				Array[j] = Array[j + 1];
-				if (j + 1 == 3)
+				if (j + 1 == *Size)
 				{
-					Array[j] = 0;
-					Size--;
+					strcpy(Array[j].name, "");
+					strcpy(Array[j].phone, "");
+					(*Size)--;
 					return;
 				}
+				else
+					Array[j] = Array[j + 1];
 			}
 		}
 	}
 	printf("해당하는 이름을 가진 사람이 없습니다.\n");
 }
 
-void ShowLinear(NameCard *Array[], int Size)
+void ShowLinear(NameCard *Array, int Size)
 {
 	for (int i = 0; i < Size; i++)
 	{
-		if (Array[i] != 0)
-			ShowNameCardInfo(Array[i]);
+		if (&Array[i] != 0)
+			ShowNameCardInfo(&Array[i]);
 	}
 }
 
@@ -110,11 +112,13 @@ int main()
 		InputInformation(CardArray, iSize);
 		iNumberOfInput++;
 	}
-	
-	SearchInformation(&CardArray, iSize);
-	SearchPhoneNumber(&CardArray, iSize);
-	DeleteInformation(&CardArray, &iSize);
-	ShowLinear(&CardArray, iSize);
+
+	SearchInformation(CardArray, iSize);
+	SearchPhoneNumber(CardArray, iSize);
+	DeleteInformation(CardArray, &iSize);
+	ShowLinear(CardArray, iSize);
+
+	free(CardArray);
 
 	return 0;
 }
