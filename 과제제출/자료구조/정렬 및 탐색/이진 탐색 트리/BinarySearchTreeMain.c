f9c1b6f1687c97da6d827e874c3e0e4694ec9dee
ParkSoHyeon <zxasd9874@gmail.com>
2020-06-22 14:56:56 +0900

    d

--- 과제제출/자료구조/정렬 및 탐색/이진 탐색 트리/BinarySearchTreeMain.c
+++ 과제제출/자료구조/정렬 및 탐색/이진 탐색 트리/BinarySearchTreeMain.c
@@ -2,13 +2,13 @@
 
 int main()
 {
-	BTreeNode NewRootNode;
-	NewRootNode = *MakeBTreeNode();
+	BTreeNode *NewRootNode;
+	BSTMakeAndInit(&NewRootNode);
 
-	int iSelect = 0;
+	int iSelect = 0, iTmp = 0;
 	while (1)
 	{
-		printf("메뉴 선택(1. 데이터 입력)\n");
+		printf("메뉴 선택(1. 데이터 입력 2. 입력된 데이터들 출력)\n");
 		printf("입력: ");
 		scanf_s("%d", &iSelect, sizeof(iSelect));
 		switch (iSelect)
@@ -17,6 +17,12 @@ int main()
 			printf("올바른 메뉴 번호 입력\n");
 			break;
 		case 1:
+			printf("입력할 데이터 입력: \n");
+			scanf_s("%d", &iTmp, sizeof(iTmp));
+			BSTInsert(&NewRootNode, iTmp);
+			break;
+		case 2:
+			break;
 		}
 		puts("");
 	}
