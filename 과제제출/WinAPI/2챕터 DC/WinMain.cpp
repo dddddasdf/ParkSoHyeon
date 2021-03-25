d6ec687ac3267ddd372e991d37d87c454bb5ff0c
dddddasdf <zxasd9874@gmail.com>
2021-03-25 14:56:40 +0900

    asdf

---
+++ 과제제출/WinAPI/2챕터 DC/WinMain.cpp
@@ -0,0 +1,118 @@
+#include<windows.h>
+#include <cmath>
+LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
+HINSTANCE g_hInst;
+LPCTSTR lpszClass = TEXT("TextOut");
+
+int APIENTRY WinMain(HINSTANCE hInstance, HINSTANCE hPervlnstance, LPSTR lpszCmdParam, int nCmdShow)
+{
+	HWND hWnd;
+	MSG Message;
+	WNDCLASS WndClass;
+	g_hInst = hInstance;
+
+	WndClass.cbClsExtra = 0;
+	WndClass.cbWndExtra = 0;
+	WndClass.hbrBackground = (HBRUSH)GetStockObject(WHITE_BRUSH);
+	WndClass.hCursor = LoadCursor(NULL, IDC_ARROW);
+	WndClass.hIcon = LoadIcon(NULL, IDI_APPLICATION);
+	WndClass.hInstance = hInstance;
+	WndClass.lpfnWndProc = WndProc;
+	WndClass.lpszClassName = lpszClass;
+	WndClass.lpszMenuName = NULL;
+	WndClass.style = CS_HREDRAW | CS_VREDRAW;
+	RegisterClass(&WndClass);
+
+	hWnd = CreateWindow(lpszClass, lpszClass, WS_OVERLAPPEDWINDOW, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, NULL, (HMENU)NULL, hInstance, NULL);
+	ShowWindow(hWnd, nCmdShow);
+
+	while (GetMessage(&Message, NULL, 0, 0))
+	{
+		TranslateMessage(&Message);
+		DispatchMessage(&Message);
+	}
+	return (int)Message.wParam;
+}
+
+LRESULT CALLBACK WndProc(HWND hWnd, UINT iMessage, WPARAM wParam, LPARAM lParam)
+{
+	HDC hdc;
+	PAINTSTRUCT ps;
+
+	switch (iMessage)
+	{
+	case WM_PAINT:
+	{
+		hdc = BeginPaint(hWnd, &ps);
+
+		SetTextAlign(hdc, TA_LEFT);
+		TextOut(hdc, 10, 10, TEXT("No.1"), sizeof("No.1") - 1);
+
+		MoveToEx(hdc, 160, 50, NULL);
+		LineTo(hdc, 120, 100);
+
+		MoveToEx(hdc, 187, 45, NULL);
+		LineTo(hdc, 147, 95);
+
+		MoveToEx(hdc, 195, 65, NULL);
+		LineTo(hdc, 155, 115);
+
+		Ellipse(hdc, 110, 110, 150, 150);
+
+		TextOut(hdc, 165, 125, TEXT("메테오~"), sizeof("메테오~") - 1);
+
+		/////////////////////////////////////////////////////////////////////////////
+		TextOut(hdc, 10, 170, TEXT("No.2"), sizeof("No.2") - 1);
+
+		//반지름은 40
+		double X, Y;	//각각 x, y 좌표
+		int Radius = 40;	//반지름
+
+		int XCentre = 80;	//중심 X좌표
+		int YCentre = 230;	//중심 Y좌표
+
+		SetPixel(hdc, XCentre, YCentre, RGB(0, 0, 0));
+
+		//점은 5도 단위로 찍는다
+		for (int Angle = 0; Angle <= 360; Angle += 5)
+		{
+			X = cos(Angle) * Radius;
+			Y = sin(Angle) * Radius;
+
+			SetPixel(hdc, XCentre + X, YCentre + Y, RGB(0, 0, 0));
+		}
+
+		/////////////////////////////////////////////////////////////////////////////
+		TextOut(hdc, 10, 280, TEXT("No.3"), sizeof("No.3") - 1);
+
+		//장축 40 단축 20
+
+		XCentre = 80;	//중심 X좌표
+		YCentre = 330;	//중심 Y좌표
+
+		SetPixel(hdc, XCentre, YCentre, RGB(0, 0, 0));
+
+		//점은 5도 단위로 찍는다
+		for (int Angle = 0; Angle <= 360; Angle += 5)
+		{
+			X = cos(Angle) * Radius;
+			Y = (sin(Angle) * Radius) / 2;
+
+			SetPixel(hdc, XCentre + X, YCentre + Y, RGB(0, 0, 0));
+		}
+
+	}
+		EndPaint(hWnd, &ps);
+		return 0;
+	case WM_DESTROY:
+		PostQuitMessage(0);
+		return 0;
+
+	case WM_LBUTTONDOWN:
+		hdc = GetDC(hWnd);
+
+		ReleaseDC(hWnd, hdc);
+		return 0;
+	}
+	return(DefWindowProc(hWnd, iMessage, wParam, lParam));
+}
